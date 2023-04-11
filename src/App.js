4c4fcfcdee3060473a52b775b78209e70d4e01ba
SShikha123 <singhalshikha1999@gmail.com>
2023-04-11 18:27:15 +0530

    modified:   package.json 	modified:   src/App.css 	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const[value,setValue]=useState("");
+  const[result,setResult]=useState("Result");
+  const OnEnterValueHandler=(e)=>{
+    let values=e.target.name;
+    let input= value+values ;
+    // let newinput=input+" " 
+    setValue(input);
+    console.log(e.target.name)
+  }
+  const OnCalculateHandler=()=>{
+    const resul=eval(value);
+    setResult(resul);
+    console.log(resul);
+  }
+  const OnResetHandler=()=>{
+setValue("enter value");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="results">
+        <h2>{result}</h2>
+        <h4>{value}</h4>
+      </div>
+      <div className="buttons">
+        <div>
+          <button name="7" className="perbutton" onClick={OnEnterValueHandler}>7</button>
+          <button name="8" className="perbutton" onClick={OnEnterValueHandler}>8</button>
+          <button name="9" className="perbutton" onClick={OnEnterValueHandler}>9</button>
+          <button name="/" className="perbutton" onClick={OnEnterValueHandler}>/</button>
+        </div>
+        <div>
+          <button name="4" className="perbutton" onClick={OnEnterValueHandler}>4</button>
+          <button name="5" className="perbutton" onClick={OnEnterValueHandler}>5</button>
+          <button name="6" className="perbutton" onClick={OnEnterValueHandler}>6</button>
+          <button name="*" className="perbutton" onClick={OnEnterValueHandler}>*</button>
+        </div>
+        <div>
+          <button name="1" className="perbutton" onClick={OnEnterValueHandler}>1</button>
+          <button name="2" className="perbutton" onClick={OnEnterValueHandler}>2</button>
+          <button name="3" className="perbutton" onClick={OnEnterValueHandler}>3</button>
+          <button name="-" className="perbutton" onClick={OnEnterValueHandler}>-</button>
+        </div>
+        <div>
+          <button name="." className="perbutton" onClick={OnEnterValueHandler}>.</button>
+          <button name="0" className="perbutton" onClick={OnEnterValueHandler}>0</button>
+          <button name="=" className="perbutton" onClick={OnCalculateHandler}>=</button>
+          <button name ="+" className="perbutton" onClick={OnEnterValueHandler}>+</button>
+        </div>
+        <div>
+          <button className="perbutton" onClick={OnResetHandler}>Reset</button>
+        </div>
+      </div>
+      <div></div>
     </div>
   );
 }
